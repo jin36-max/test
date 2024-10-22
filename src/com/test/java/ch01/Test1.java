97f6a52d0f00718445b6ab6cf7b47a80c88439ca
jin36 <cjdanaud@gmail.com>
2024-10-22 09:15:33 +0900

    복구

--- src/com/test/java/ch01/Test1.java
+++ src/com/test/java/ch01/Test1.java
@@ -2,7 +2,7 @@ package com.test.java.ch01;
 
 public class Test1 {
 	public static void main(String[] arga) {
-		System.out.println("엘컴퓨터학원에 오신 것을 환영합니다.111");
+		System.out.println("엘컴퓨터학원에 오신 것을 환영합니다.");
 	}
 
 }
