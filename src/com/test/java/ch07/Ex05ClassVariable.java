cda51bcbbd76fdf1c9d0043a3c4cfdeba9f1cb60
jin36 <cjdanaud@gmail.com>
2024-11-01 11:48:31 +0900

    저장

---
+++ src/com/test/java/ch07/Ex05ClassVariable.java
@@ -0,0 +1,9 @@
+package com.test.java.ch07;
+
+public class Ex05ClassVariable {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/test/java/ch07/Ex05ClassVariableTest.java
@@ -0,0 +1,9 @@
+package com.test.java.ch07;
+
+public class Ex05ClassVariableTest {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
